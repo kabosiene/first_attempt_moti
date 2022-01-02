927f7b8dee69fd248c4d9554b109a4a12b78b0cc
Kristina <kristina.kabosiene@poppri.com>
2022-01-02 16:20:14 +0200

    Add users

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   resources :posts do
     resources :comments
   end
-
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
   # Defines the root path route ("/")
   root "main#index"
 end
